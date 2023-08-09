b84e4b437c37f91e5a881668c1575502757426f5
LynnG19 <antiphreeeze@gmail.com>
2023-08-09 14:56:38 -0500

    its done

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 // Write your JavaScript code here!
-
+//const { myFetch } = require("./scriptHelper");
 window.addEventListener("load", function() {
+   
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+       let selectedPlanet = pickPlanet(listedPlanets);
+       let name = selectedPlanet.name;
+       let diameter = selectedPlanet.diameter;
+       let star = selectedPlanet.star;
+       let distance = selectedPlanet.distance;
+       let moons = selectedPlanet.moons;
+       let imageUrl = selectedPlanet.image;
+       addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
+    })
+
+       let form = document.querySelector("form");
+          form.addEventListener("submit", function(event) {
+       event.preventDefault();
+       let list = document.getElementById("faultyItems");
+       let pilot = document.querySelector("input[name=pilotName]").value;
+       let copilot = document.querySelector("input[name=copilotName]").value;
+       let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+       let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+       list.style.visibility = "hidden";
+   
+     formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel); 
+   
+      
+      });
+       
+    // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+   
+
+
    
 });
